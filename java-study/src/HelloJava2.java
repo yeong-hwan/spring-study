4829ce82b42570c03d7c73ef218c30cc4fd9edf2
yeong-hwan <jghff700@naver.com>
2024-01-04 20:58:27 +0900

    chore: basic setting for spring boot package

---
+++ java-study/src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("hello java 1");
+        System.out.println("hello java 2");
+        System.out.println("hello java 3");
+
+    }
+}
---
+++ java-study/src/variable/Var1.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+
+    }
+}
